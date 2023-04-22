37795b31e20d13a9f06779c99a8e430f27211e23
Rishav Anand <rishavanand2k21@gmail.com>
2023-04-22 18:55:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
+import './index.css'
+
+function App() {
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  )
+}
+
+export default App
